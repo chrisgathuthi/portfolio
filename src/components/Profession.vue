4e68e5b78f294e3f92b1f51b6853b60774cce4f8
Saint-Chris-7 <chrisgathuthi@gmail.com>
2023-07-28 09:35:53 +0300

    about me

--- src/components/Profession.vue
+++ src/components/Profession.vue
@@ -1,8 +1,57 @@
 <template>
     <section>
-        <div class="main-area">
-            <h1>Hello world</h1>
+        <div class="about-me">
+            <div class="about-heading">
+                <h1>About me</h1>
+            </div>
         </div>
+        <div class="self-summary">
+            <div class="self-image">
+                <img src="./images/chris-bg.jpg" alt="christopher gathuthi image">
+            </div>
+            <div class="main-area">
+                <div class="cta-wrapper">
+                    <p>I am web developer with almost three years of experience in developing websites and web applications.
+                        I
+                        build web applications that aim at streamlining business operations and creating value for business
+                    </p>
+                </div>
+                <div class="stats">
+                    <div class="box">
+                        <h2>02+</h2>
+                        <span>Years experience</span>
+                    </div>
+                    <div class="box">
+                        <h2>20+</h2>
+                        <span>completed projects</span>
+                    </div>
+                    <div class="box">
+                        <h2>10+</h2>
+                        <span>Clients</span>
+                    </div>
+                </div>
+                <div class="resume">
+                    <a href="./images/" role="button" download>Download Resume</a>
+                </div>
+            </div>
+
+        </div>
+
     </section>
 
+    <section>
+        <div class="services-wrapper">
+            <div class="services-heading">
+                <h3>Services</h3>
+                <div class="service-items">
+                    <div class="web-apps">
+                        <img src="" alt="">
+                        <span>web applications</span>
+                    </div>
+                    .backend-web
+                </div>
+            </div>
+        </div>
+
+    </section>
 </template>
---
+++ src/components/Services.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="skills">
+        <ul>
+            <li><i class="fa-brands fa-python" style="color: blue;"></i></li>
+            <li><i class="fa-brands fa-square-js" style="color: yellow;"></i></li>
+            <li><i class="fa-brands fa-golang" style="color: #00A7D0;"></i></li>
+            <li><i class="fa-brands fa-github"></i></li>
+            <li><i class="fa-solid fa-database"></i></li>
+            <li><i class="fa-brands fa-docker" style="color: #00A7D0;"></i></li>
+            <li><i class="fa-brands fa-ubuntu"></i></li>
+            <li>Django</li>
+            <li>Vue</li>
+            <li>React Native</li>
+        </ul>
+    </div>
+</template>
