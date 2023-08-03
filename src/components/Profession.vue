dcb011f683eeb465c019faa663f72d272094463c
Saint-Chris-7 <chrisgathuthi@gmail.com>
2023-08-03 12:19:23 +0300

    mobile responsive

--- src/components/Profession.vue
+++ src/components/Profession.vue
@@ -38,65 +38,67 @@
         </div>
 
     </section>
-    <div class="skills">
-        <div class="skills-heading">
-            <h1>Skills</h1>
-        </div>
-    </div>
-    <div class="skills-wrapper">
-        <div class="tools-wrapper">
-            <div class="tool-item">
-                <img src="./images/python.png" alt="django project icon" width="64" height="64">
-                <span>Python</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/JavaScript.png" alt="django project icon" width="64" height="64">
-                <span>JavaScript</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/go.png" alt="django project icon" width="64" height="64">
-                <span>Go</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/django.png" alt="django project icon" width="64" height="64">
-                <span>Django</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/vue.png" alt="django project icon" width="64" height="64">
-                <span>Vue</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/postgres.png" alt="django project icon" width="64" height="64">
-                <span>Postgres</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/docker.png" alt="django project icon" width="64" height="64">
-                <span>Docker</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/git.png" alt="django project icon" width="64" height="64">
-                <span>Git</span>
-            </div>
-            <div class="tool-item">
-                <img src="./images/linux.png" alt="django project icon" width="64" height="64">
-                <span>bash</span>
+    <section>
+        <div class="skills">
+            <div class="skills-heading">
+                <h1>Skills</h1>
             </div>
         </div>
-        <div class="services-wrapper">
-            <div class="service-item">
-                <!-- <img src="./images/web-dev.png" alt="full stack web developer"> -->
-                <span>Full-stack Web developer</span>
+        <div class="skills-wrapper">
+            <div class="tools-wrapper">
+                <div class="tool-item">
+                    <img src="./images/python.png" alt="django project icon" width="64" height="64">
+                    <span>Python</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/JavaScript.png" alt="django project icon" width="64" height="64">
+                    <span>JavaScript</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/go.png" alt="django project icon" width="64" height="64">
+                    <span>Go</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/django.png" alt="django project icon" width="64" height="64">
+                    <span>Django</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/vue.png" alt="django project icon" width="64" height="64">
+                    <span>Vue</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/postgres.png" alt="django project icon" width="64" height="64">
+                    <span>Postgres</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/docker.png" alt="django project icon" width="64" height="64">
+                    <span>Docker</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/git.png" alt="django project icon" width="64" height="64">
+                    <span>Git</span>
+                </div>
+                <div class="tool-item">
+                    <img src="./images/linux.png" alt="django project icon" width="64" height="64">
+                    <span>bash</span>
+                </div>
             </div>
-            <div class="service-item">
-                <!-- <img src="./images/server.png" alt="server icon"> -->
-                <span>Back-end Developer</span>
-            </div>
-            <div class="service-item">
-                <!-- <img src="./images/front-end.jpeg" alt="frontend icon"> -->
-                <span>Front-end Developer</span>
+            <div class="services-wrapper">
+                <div class="service-item">
+                    <!-- <img src="./images/web-dev.png" alt="full stack web developer"> -->
+                    <span>Full-stack Web developer</span>
+                </div>
+                <div class="service-item">
+                    <!-- <img src="./images/server.png" alt="server icon"> -->
+                    <span>Back-end Developer</span>
+                </div>
+                <div class="service-item">
+                    <!-- <img src="./images/front-end.jpeg" alt="frontend icon"> -->
+                    <span>Front-end Developer</span>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <section>
         <div class="portfolio">
@@ -149,22 +151,24 @@
     </section>
     <section>
         <div class="articles">
-        <div class="articles-heading">
-            <h1>Articles</h1>
-        </div>
-        <div class="articles-wrapper">
-            <div class="articles-item">
-                <img src="./images/django-orm.png" alt="">
-                <h1><a href="https://medium.com/@soverignchriss/querying-related-objects-in-django-web-framework-3aece5bd927d">Read</a> </h1>
+            <div class="articles-heading">
+                <h1>Articles</h1>
+            </div>
+            <div class="articles-wrapper">
+                <div class="articles-item">
+                    <img src="./images/django-orm.png" alt="">
+                    <h1><a
+                            href="https://medium.com/@soverignchriss/querying-related-objects-in-django-web-framework-3aece5bd927d">Read</a>
+                    </h1>
+                </div>
             </div>
         </div>
-    </div>
     </section>
     <div class="events">
-            <div class="events-heading">
-                <h1>Hackathon & Events</h1>
-            </div>
+        <div class="events-heading">
+            <h1>Hackathon & Events</h1>
         </div>
+    </div>
     <section>
 
 
